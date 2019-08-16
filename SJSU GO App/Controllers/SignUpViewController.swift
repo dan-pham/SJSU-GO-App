ab912c2535179ba6af4b51f9bcb7ce802c9dca2c
Dan Pham <phamdan.sj@gmail.com>
2019-08-15 18:25:31 -0700

    update: Change drop down to pickerView

--- SJSU GO App/Controllers/SignUpViewController.swift
+++ SJSU GO App/Controllers/SignUpViewController.swift
@@ -13,8 +13,9 @@ class SignUpViewController: UIViewController {
 
     // MARK: Properties
     
-    var majorButton = DropDownButton()
-    var majorButtonConstraints = [NSLayoutConstraint]()
+    let majors = ["Aerospace Engineering", "Aviation", "Biomedical Engineering", "Chemical Engineering", "Civil Engineering", "Computer Engineering", "Electrical Engineering", "Engineering Management", "General Engineering", "Human Factor/Ergonomics", "Industrial and Systems Engineering", "Industrial Technology",  "Materials Engineering", "Mechanical Engineering", "Software Engineering"]
+    let academicYears = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
+    var selectedFieldData = [String]()
     
     // MARK: Outlets
     
@@ -23,50 +24,30 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var majorTextField: UITextField!
+    @IBOutlet weak var academicYearTextField: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
     @IBOutlet weak var cancelButton: UIBarButtonItem!
-    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signUpButton: UIBarButtonItem!
     
     // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         delegateTextFields()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-//        configureMajorButton()
+        configurePickerView()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(true)
         clearTextFields()
-        
-//        NSLayoutConstraint.deactivate(majorButtonConstraints)
-//        majorButton.dropView.removeFromSuperview()
-//        self.majorButton.removeFromSuperview()
-//        majorButton.removeFromSuperview()
     }
     
-    func configureMajorButton() {
-        majorButton = DropDownButton.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        majorButton.setTitle("Major", for: .normal)
-        majorButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.view.addSubview(majorButton)
-        let centerXAnchor = majorButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
-        let centerYAnchor = majorButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        let widthAnchor = majorButton.widthAnchor.constraint(equalToConstant: 150)
-        let heightAnchor = majorButton.heightAnchor.constraint(equalToConstant: 40)
-        
-        majorButtonConstraints.append(contentsOf: [centerXAnchor, centerYAnchor, widthAnchor, heightAnchor])
-        NSLayoutConstraint.activate(majorButtonConstraints)
-        
-        majorButton.dropView.dropDownOptions = ["Aerospace Engineering", "Computer Engineering", "Electrical Engineering", "Software Engineering"]
-    }
-    
-    func configureAcademicYearButton() {
-        
+    func configurePickerView() {
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.isHidden = true
     }
     
     // MARK: Actions
@@ -77,12 +58,12 @@ class SignUpViewController: UIViewController {
     
     @IBAction func signUp(_ sender: Any) {
         
-        guard idTextField.hasText && passwordTextField.hasText && emailTextField.hasText && firstNameTextField.hasText && lastNameTextField.hasText else {
+        guard idTextField.hasText && passwordTextField.hasText && emailTextField.hasText && firstNameTextField.hasText && lastNameTextField.hasText && majorTextField.hasText && academicYearTextField.hasText else {
             print("Please fill in every text field except for the major and academic year.")
             return
         }
         
-        guard let sjsuId = idTextField.text, let sjsuPassword = passwordTextField.text, let sjsuEmail = emailTextField.text, let firstName = firstNameTextField.text, let lastName = lastNameTextField.text else {
+        guard let sjsuId = idTextField.text, let sjsuPassword = passwordTextField.text, let sjsuEmail = emailTextField.text, let firstName = firstNameTextField.text, let lastName = lastNameTextField.text, let major = majorTextField.text, let academicYear = academicYearTextField.text else {
             print("Sign up failed")
             return
         }
@@ -98,7 +79,7 @@ class SignUpViewController: UIViewController {
                 return
             }
             
-            let values = ["sjsu_id": sjsuId, "sjsu_email": sjsuEmail, "sjsu_password": sjsuPassword, "first_name": firstName, "last_name": lastName]
+            let values = ["user_id": uid, "sjsu_id": sjsuId, "sjsu_email": sjsuEmail, "first_name": firstName, "last_name": lastName, "major": major, "academic_year": academicYear]
             
             self.registerUserIntoDatabaseWithUID(uid, values: values as [String : AnyObject])
         }
@@ -120,15 +101,18 @@ class SignUpViewController: UIViewController {
     }
 }
 
-// MARK: - UITextFieldDelegate
+// MARK: - UITextField
 
 extension SignUpViewController: UITextFieldDelegate {
+    
     func delegateTextFields() {
         idTextField.delegate = self
         passwordTextField.delegate = self
         emailTextField.delegate = self
         firstNameTextField.delegate = self
         lastNameTextField.delegate = self
+        majorTextField.delegate = self
+        academicYearTextField.delegate = self
     }
     
     func clearTextFields() {
@@ -137,171 +121,60 @@ extension SignUpViewController: UITextFieldDelegate {
         emailTextField.text = ""
         firstNameTextField.text = ""
         lastNameTextField.text = ""
+        majorTextField.text = ""
+        academicYearTextField.text = ""
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         return textField.resignFirstResponder()
     }
-}
-
-// MARK: - DropDownButton
-
-protocol DropDownProtocol {
-    func dropDownPressed(string: String)
-}
-
-class DropDownButton: UIButton, DropDownProtocol {
     
-    func dropDownPressed(string: String) {
-        self.setTitle(string, for: .normal)
-        self.dismissDropDown()
-    }
-    
-    var dropView = DropDownView()
-    var height = NSLayoutConstraint()
-    var dropViewConstraints = [NSLayoutConstraint]()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        self.backgroundColor = UIColor.darkGray
-        
-        dropView = DropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
-        dropView.delegate = self
-        dropView.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    deinit {
-        NSLayoutConstraint.deactivate(dropViewConstraints)
-        dropView.removeFromSuperview()
-        self.dropView.removeFromSuperview()
-        self.removeFromSuperview()
-    }
-    
-    override func didMoveToSuperview() {
-        self.superview?.addSubview(dropView)
-        self.superview?.bringSubviewToFront(dropView)
-        
-        let topAnchor = dropView.topAnchor.constraint(equalTo: self.bottomAnchor)
-        let centerXAnchor = dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
-        let widthAnchor = dropView.widthAnchor.constraint(equalTo: self.widthAnchor)
-        height = dropView.heightAnchor.constraint(equalToConstant: 0)
-        
-        dropViewConstraints.append(contentsOf: [topAnchor, centerXAnchor, widthAnchor, height])
-        NSLayoutConstraint.activate(dropViewConstraints)
-    }
-    
-    var isOpen = false
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if isOpen == false {
-            isOpen = true
-            
-            NSLayoutConstraint.deactivate([self.height])
-            
-            if self.dropView.tableView.contentSize.height > 150 {
-                self.height.constant = 150
-            } else {
-                self.height.constant = self.dropView.tableView.contentSize.height
-            }
-            
-            NSLayoutConstraint.activate([self.height])
-            
-            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
-                self.dropView.layoutIfNeeded()
-                self.dropView.center.y += self.dropView.frame.height / 2
-            }, completion: nil)
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (textField == majorTextField) {
+            selectedFieldData = majors
+            pickerView.reloadAllComponents()
+            pickerView.isHidden = false
+            textField.endEditing(true)
+        } else if (textField == academicYearTextField) {
+            selectedFieldData = academicYears
+            pickerView.reloadAllComponents()
+            pickerView.isHidden = false
+            textField.endEditing(true)
         } else {
-            isOpen = false
-            
-            NSLayoutConstraint.deactivate([self.height])
-            self.height.constant = 0
-            NSLayoutConstraint.activate([self.height])
-            
-            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
-                self.dropView.center.y -= self.dropView.frame.height / 2
-                self.dropView.layoutIfNeeded()
-            }, completion: nil)
+            pickerView.isHidden = true
         }
     }
     
-    func dismissDropDown() {
-        isOpen = false
-        
-        NSLayoutConstraint.deactivate([self.height])
-        self.height.constant = 0
-        NSLayoutConstraint.activate([self.height])
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
-            self.dropView.center.y -= self.dropView.frame.height / 2
-            self.dropView.layoutIfNeeded()
-        }, completion: nil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
 }
 
-class DropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+// MARK: - UIPickerView
+
+extension SignUpViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
-    var dropDownOptions = [String]()
-    var tableView = UITableView()
-    var delegate: DropDownProtocol!
-    var dropDownConstraints = [NSLayoutConstraint]()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        tableView.backgroundColor = UIColor.white
-        self.backgroundColor = UIColor.white
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.addSubview(tableView)
-        
-        let leftAnchor = tableView.leftAnchor.constraint(equalTo: self.leftAnchor)
-        let rightAnchor = tableView.rightAnchor.constraint(equalTo: self.rightAnchor)
-        let topAnchor = tableView.topAnchor.constraint(equalTo: self.topAnchor)
-        let bottomAnchor = tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-        
-        dropDownConstraints.append(contentsOf: [leftAnchor, rightAnchor, topAnchor, bottomAnchor])
-        NSLayoutConstraint.activate(dropDownConstraints)
-    }
-    
-    deinit {
-        NSLayoutConstraint.deactivate(dropDownConstraints)
-        tableView.removeFromSuperview()
-        self.tableView.removeFromSuperview()
-        self.removeFromSuperview()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dropDownOptions.count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return selectedFieldData.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
-        
-        cell.textLabel?.text = dropDownOptions[indexPath.row]
-        cell.backgroundColor = UIColor.white
-        
-        return cell
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return selectedFieldData[row]
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.delegate.dropDownPressed(string: dropDownOptions[indexPath.row])
-        self.tableView.deselectRow(at: indexPath, animated: true)
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if selectedFieldData == majors {
+            majorTextField.text = majors[row]
+        } else if selectedFieldData == academicYears {
+            academicYearTextField.text = academicYears[row]
+        }
+        
+        perform(#selector(hidePickerView), with: nil, afterDelay: 1)
     }
+    
+    @objc func hidePickerView() {
+        pickerView.isHidden = true
+    }
+    
 }
